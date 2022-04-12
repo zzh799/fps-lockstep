60280d80484f97d8b81fe502aa2d61199d8d63cf
zeaven <799828242@qq.com>
2022-04-12 11:01:02 +0800

    add fairyGUI

---
+++ Client/Assets/Scripts/UI_Gen/basic/WndLogin.cs
@@ -0,0 +1,39 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+using FairyGUI.Utils;
+
+namespace basic
+{
+    public partial class WndLogin : GComponent
+    {
+        public Controller ctrlState;
+        public GTextInput txtUserName;
+        public GTextInput txtPassword;
+        public GButton btnLogin;
+        public GButton btnRegister;
+        public GTextInput txtPassword2;
+        public GButton btnReturnLogin;
+        public GButton btnRegisterConfirm;
+        public const string URL = "ui://93tyfp33v3wk0";
+
+        public static WndLogin CreateInstance()
+        {
+            return (WndLogin)UIPackage.CreateObject("basic", "WndLogin");
+        }
+
+        public override void ConstructFromXML(XML xml)
+        {
+            base.ConstructFromXML(xml);
+
+            ctrlState = GetController("ctrlState");
+            txtUserName = (GTextInput)GetChild("txtUserName");
+            txtPassword = (GTextInput)GetChild("txtPassword");
+            btnLogin = (GButton)GetChild("btnLogin");
+            btnRegister = (GButton)GetChild("btnRegister");
+            txtPassword2 = (GTextInput)GetChild("txtPassword2");
+            btnReturnLogin = (GButton)GetChild("btnReturnLogin");
+            btnRegisterConfirm = (GButton)GetChild("btnRegisterConfirm");
+        }
+    }
+}
---
+++ Client/Assets/Scripts/UI_Gen/basic/basicBinder.cs
@@ -0,0 +1,14 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace basic
+{
+    public class basicBinder
+    {
+        public static void BindAll()
+        {
+            UIObjectFactory.SetPackageItemExtension(WndLogin.URL, typeof(WndLogin));
+        }
+    }
+}
